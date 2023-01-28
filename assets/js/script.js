7e41806afe54c428e253340a2ad99c9617e9457e
Hayley <hb_060492@hotmail.co.uk>
2023-01-28 18:00:18 +0000

    AJAX calls to find city coordinates, then use coordinates to get weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,23 @@
-// Link to API key 
-var APIKey = "";
+var APIKey = "e74bf8be05c5eedb9d4a56d89339e903";
 
+//TODO city variable should be whatever user inputs
 var city = "London";
 
-// The URL for the query response
-var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + APIKey;
-
-// AJAX call
+// AJAX call to find the city's co-ordinates
 $.ajax({
-  url: queryURL,
+  url: "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + APIKey,
   method: "GET"
 }).then(function(response) {
-    console.log(response);
+    var cityLat = response[0].lat;
+    var cityLon = response[0].lon;
+    console.log(cityLat);
+    console.log(cityLon);
+    //AJAX call using the coordinates to get the forecast data
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" + cityLon + "&appid=" + APIKey,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+    })
+
 })
