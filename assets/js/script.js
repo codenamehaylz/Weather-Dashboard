192420c729b65cf081a8bc1a28811bce1030d9f3
Hayley <hb_060492@hotmail.co.uk>
2023-01-28 19:00:16 +0000

    function to get the midday weather data for next 5 days

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,37 @@ $.ajax({
 }).then(function(response) {
     var cityLat = response[0].lat;
     var cityLon = response[0].lon;
-    console.log(cityLat);
-    console.log(cityLon);
     //AJAX call using the coordinates to get the forecast data
     $.ajax({
       url: "http://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" + cityLon + "&appid=" + APIKey,
       method: "GET"
     }).then(function(response) {
       console.log(response);
+      var todayArr = response.list[0];
+      var cityName = response.city.name;
+      var currentDate = todayArr.dt_txt;
+      var currentIconCode = todayArr.weather[0].icon;
+      var currentTemp = (todayArr.main.temp - 273.15).toFixed(1);
+      var currentHum = todayArr.main.humidity;
+      var currentWind = todayArr.wind.speed;
+      var currentData = [cityName, currentDate, currentIconCode, currentTemp, currentHum, currentWind];
+      for (var i=0; i<currentData.length; i++){
+        var container = $('<p>');
+        container.append(currentData[i]);
+        $('#today').append(container);
+      }
+
+      getFiveDays(response);
     })
 
 })
+
+//function to retrieve the midday weather data for the next 5 days
+function getFiveDays(dataArr) {
+  for (var i=0; i<dataArr.list.length; i++){
+    var date = dataArr.list[i].dt_txt;
+    if (date.includes("12:00:00")){
+      console.log(dataArr.list[i]);
+    }
+  }
+}
